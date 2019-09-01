b93006d1a55338cd9365682403e5cf6a6709fc75
Victor Shermon <victor804.gt@gmail.com>
2019-09-01 09:51:36 -0300

    Adicionando primeiros arquivos javascript

---
+++ assets/_js/ManipulationDom.js
@@ -0,0 +1,8 @@
+export class ManipulationDom {
+
+ constructor(element) {
+   this.element =  document.querySelectorAll(element);
+   return this.element;
+ }
+
+}
---
+++ assets/_js/script.js
@@ -0,0 +1,4 @@
+import {ManipulationDom as shermon} from './ManipulationDom.js';
+
+let efeitos = new shermon('html');
+console.log(efeitos);
